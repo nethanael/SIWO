d0a330566dbbdfef2aaf5bcc65d4964c0b8b750d
Pablo Hidalgo <pablohidalgo.1983@gmail.com>
2022-12-08 18:23:00 -0600

    Modify Server Params- Security

--- includes/server_params.php
+++ includes/server_params.php
@@ -6,11 +6,11 @@
     $password = 'MSRdbbqkDyQ9jKWh'; 	
     $myDB = 'trasmedi_sistema';
 
-    //datos en el server real
+    //datos en el server real -> eliminados para evitar riesgo de seguridad
 
     //$servername = 'localhost';
-    //$username = 'trasmedi_usuario';
-    //$password = '?QG6)CNcm[q^'; 
-    //$myDB = 'trasmedi_sistema';
+    //$username = 'eliminado';
+    //$password = 'eliminado'; 
+    //$myDB = 'eliminado';
     
 ?>
